e3f4ce7a941329238f20516c64b2909f22b17afd
The Merchant <phathwa@gmail.com>
2024-10-30 14:44:16 +0200

    port 8000

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -1,5 +1,5 @@
 # gunicorn.config.py
 
-bind = "0.0.0.0:5000"  # or any port you want
+bind = "0.0.0.0:8000"  # or any port you want
 workers = 4  # Adjust based on your server capabilities
 worker_class = "uvicorn.workers.UvicornWorker"
